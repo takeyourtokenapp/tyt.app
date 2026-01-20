c093682f77e6358f2bf717aafebd3bf40ee59a39
takeyourtokenapp <olekfribel@icloud.com>
2026-01-20 15:34:18 +0200

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath, URL } from 'url';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, './src')
     }
   },
   define: {
     'global': 'globalThis'
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom']
   },
   build: {
     outDir: 'dist',
